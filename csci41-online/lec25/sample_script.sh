32625255793dc1d9b4e43bbf39366a39930cf736
Lawton Nichols <lawtonnichols@gmail.com>
2026-08-14 18:00:47 -0700

    .

---
+++ csci41-online/lec25/sample_script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# this is a comment in the shell scripting language!
+# every line in this file is a terminal command
+echo "Hi mom!" > file.txt
+cat file.txt
+cp file.txt file_copy.txt
